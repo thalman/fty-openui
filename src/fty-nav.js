4c780dbc1e2d4917ec32b2a0f39451022871301b
Tomas Halman <tomas@halman.net>
2017-03-25 22:02:48 +0100

    Problem: navbar is not collapsing properly
    Solution: fix missing part
    
    Signed-off-by: Tomas Halman <Tomas@Halman.net>

--- src/fty-nav.js
+++ src/fty-nav.js
@@ -8,12 +8,18 @@ var navigation = (function () {
     var render = function () {
         return (
             '<nav class="navbar navbar-default">' +
-	        '  <div class="container-fluid">' +
-	        '    <div class="navbar-header">' +
-	        '      <a class="navbar-brand" href="#">Fty</a>' +
-	        '    </div>' +
-	        '    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
-	        '      <ul class="nav navbar-nav">' +
+            '  <div class="container-fluid">' +
+            '    <div class="navbar-header">' +
+            '      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#fty-navbar-collapse" aria-expanded="false">' +
+            '        <span class="sr-only">Toggle navigation</span>' +
+            '        <span class="icon-bar"></span>' +
+            '        <span class="icon-bar"></span>' +
+            '        <span class="icon-bar"></span>' +
+            '      </button>' +
+            '      <a class="navbar-brand" href="#">Fty</a>' +
+            '    </div>' +
+            '    <div class="collapse navbar-collapse" id="fty-navbar-collapse">' +
+            '      <ul class="nav navbar-nav">' +
             '        <li class="dropdown">' +
             '          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"' +
             '             aria-haspopup="true" aria-expanded="false" id="navbardc">Datacenter<span class="caret"></span></a>' +
@@ -28,13 +34,13 @@ var navigation = (function () {
             '        </li>'+
             '        <li><a href="#/assets" onclick="ftyDrawPage(\'#/assets\'); return true;">Assets</a></li>'+
             '        <li><a href="#/alerts" onclick="ftyDrawPage(\'#/alerts\'); return true;">Alerts</a></li>'+
-	        '      </ul>' +
+            '      </ul>' +
             '      <ul class="nav navbar-nav navbar-right">' + // what is on right
             '        <li><a href="#/logout" id="navbarLogout">Logout</a></li>' +
             '      </ul>' +
-	        '    </div>' +
-	        '  </div>' +
-	        '</nav>'
+            '    </div>' +
+            '  </div>' +
+            '</nav>'
         );
     };
 
