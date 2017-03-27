1a60ff91f1b767b350d6ae26412bb0d52fde4a29
Tomas Halman <TomasHalman@eaton.com>
2017-03-27 15:29:25 +0200

    Problem: Need export CSV
    Solution: Create button on settings page and implement export
    
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

--- src/fty-settings.js
+++ src/fty-settings.js
@@ -5,13 +5,39 @@ function newSettingsPage () {
         var show = function () {
             active = true;
             $("#content").html (
-                '<div class="container" id="alertList">Comming soon</div>'
+                render()
             );
-        };
+            $("#settingsExport").click (onExportClick);
+        }
+
         var hide = function () {
             active = false;
-        };
+        }
 
+        var render = function () {
+            return (
+                '<div class="container" id="alertList">' +
+                '  <div class="row">'+
+                '    <a href="#/settings/export" class="btn btn-default" id="settingsExport">Export</a>' +
+                '  </div>' +
+                '</div>'
+            );
+        }
+
+        var onExportClick = function () {
+            $.get('/api/v1/asset/export', null, function(retData) {
+                console.log (retData);
+                var a = window.document.createElement('a');
+                a.href = window.URL.createObjectURL(new Blob([retData], {type: 'text/csv'}));
+                a.download = window.location.hostname + '.csv';
+                // Append anchor to body.
+                document.body.appendChild(a)
+                a.click();
+                // Remove anchor from body
+                document.body.removeChild(a)
+            });
+            return false;
+        }
 
         // return public api
         return {
